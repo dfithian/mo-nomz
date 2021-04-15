6666b92e29075722f1626881013145728ced2236
Dan Fithian <dan.fithian@simspace.com>
2021-04-15 12:31:42 -0400

    Fix tests

--- server/mo-nomz/test/UnitSpec.hs
+++ server/mo-nomz/test/UnitSpec.hs
@@ -21,10 +21,10 @@ spec = describe "Unit" $ do
   it "orders correctly" $ forAll (shuffle allUnits) $ \xs ->
     sortBy unitOrdering xs `shouldBe` allUnits
   it "combines quantities - cup equivalent to ounce" $
-    combineQuantities (mapFromList [(cup, ([1], 8)), (ounce, ([2], 1))]) `shouldBe` mapFromList [(ounce, ([1, 2], ReadableQuantity (Just 2) Nothing))]
+    combineQuantities (mapFromList [(cup, ([1 :: Int], 8)), (ounce, ([2], 1))]) `shouldBe` mapFromList [(ounce, ([1, 2], ReadableQuantity (Just 2) Nothing))]
   it "combines quantities - cup less than ounce" $
-    combineQuantities (mapFromList [(cup, ([1], 4)), (ounce, ([2], 1))]) `shouldBe` mapFromList [(ounce, ([1, 2], ReadableQuantity (Just 1) (Just (ReadableFraction 1 2))))]
+    combineQuantities (mapFromList [(cup, ([1 :: Int], 4)), (ounce, ([2], 1))]) `shouldBe` mapFromList [(ounce, ([1, 2], ReadableQuantity (Just 1) (Just (ReadableFraction 1 2))))]
   it "combines quantities - teaspoons to tablespoons to cups" $
-    combineQuantities (mapFromList [(teaspoon, ([1], 2)), (tablespoon, ([2], 2)), (cup, ([3], 4))]) `shouldBe` mapFromList [(cup, ([1, 2, 3], ReadableQuantity (Just 4) (Just (ReadableFraction 1 4))))]
+    combineQuantities (mapFromList [(teaspoon, ([1 :: Int], 2)), (tablespoon, ([2], 2)), (cup, ([3], 4))]) `shouldBe` mapFromList [(cup, ([1, 2, 3], ReadableQuantity (Just 4) (Just (ReadableFraction 1 4))))]
   it "retains quantities not in known conversions" $
-    combineQuantities (mapFromList [(ounce, ([1], 1)), (Unit "whole", ([2], 1))]) `shouldBe` mapFromList [(ounce, ([1], ReadableQuantity (Just 1) Nothing)), (Unit "whole", ([2], ReadableQuantity (Just 1) Nothing))]
+    combineQuantities (mapFromList [(ounce, ([1 :: Int], 1)), (Unit "whole", ([2], 1))]) `shouldBe` mapFromList [(ounce, ([1], ReadableQuantity (Just 1) Nothing)), (Unit "whole", ([2], ReadableQuantity (Just 1) Nothing))]
