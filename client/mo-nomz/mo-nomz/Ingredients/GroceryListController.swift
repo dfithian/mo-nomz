b920bfffaf2109d269c46585de8c2ee706386e57
Dan Fithian <dan.fithian@simspace.com>
2021-04-14 21:45:31 -0400

    Export ingredients

--- client/mo-nomz/mo-nomz/Ingredients/GroceryListController.swift
+++ client/mo-nomz/mo-nomz/Ingredients/GroceryListController.swift
@@ -10,11 +10,23 @@ import UIKit
 class GroceryListController: UIViewController {
     var ingredientVc: IngredientListController? = nil
     
+    @IBAction func export(_ sender: Any?) {
+        var allIngredients: [IngredientWithStartingIndex] = ((ingredientVc?.ingredients ?? []) + (ingredientVc?.bought ?? []))
+        allIngredients.sort()
+        let exportText: [String] = allIngredients.map({ (x: IngredientWithStartingIndex) -> String in
+            let y = x.ingredient.ingredient
+            return "\(y.quantity.render()) \(y.unit) \(y.name)"
+        })
+        let vc = UIActivityViewController(activityItems: exportText, applicationActivities: nil)
+        present(vc, animated: true, completion: nil)
+    }
+    
     private func loadIngredients() {
         let completion = { [weak self] (resp: ListIngredientResponse) -> Void in
             self?.ingredientVc?.ingredients = zip(resp.ingredients, (0...resp.ingredients.count)).map {
                 IngredientWithStartingIndex(ingredient: $0.0, startingIndex: $0.1)
             }
+            self?.ingredientVc?.bought = []
             DispatchQueue.main.async {
                 self?.ingredientVc?.table.reloadData()
             }
