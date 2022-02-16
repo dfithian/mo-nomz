9aeb3677443f653e769019c90ec64c19447d22f9
Dan Fithian <daniel.m.fithian@gmail.com>
2022-02-15 20:49:28 -0500

    Don't ping when app enters foreground

--- client/app/mo-nomz/SceneDelegate.swift
+++ client/app/mo-nomz/SceneDelegate.swift
@@ -84,7 +84,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        window?.rootViewController?.pingUser(completion: nil)
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
