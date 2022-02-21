3419c7e766770fb43b9b08b814cc511d3c32fe12
Dan Fithian <daniel.m.fithian@gmail.com>
2022-02-21 14:08:56 -0500

    More compact

--- client/web/src/routes/recipe.js
+++ client/web/src/routes/recipe.js
@@ -28,32 +28,15 @@ const RecipePage = () => {
             <div className="column is-4 is-offset-2">
               <Centered>
                 <div id="icon">
-                  <PlainImg image={icon} size="is-128x128" alt="Nomz" />
+                  <PlainImg image={icon} size="is-96x96" alt="Nomz" />
                 </div>
               </Centered>
             </div>
             <div className="column is-4">
               <H3>Skip the ads and life story.</H3>
               <H4>Save recipes and instantly create grocery lists. Simple, easy, and fun!</H4>
-            </div>
-          </Row>
-          <div className="column is-4 is-offset-6">
-            <Centered>
-              <ImgLink href="https://apps.apple.com/us/app/grocer-ez/id1563273742" size="is-128x128" image={appStore} alt="Download on the App Store" />
-            </Centered>
-          </div>
-          <Row>
-            <div className="column is-8 is-offset-2">
-              <Centered>
-                <H5>You will be redirected to <b>{domain}</b> in 3 seconds</H5>
-              </Centered>
-            </div>
-          </Row>
-          <Row>
-            <div className="column is-8 is-offset-2">
-              <Centered>
-                <p>If you don't want to wait, <a href={url}>click here</a>.</p>
-              </Centered>
+              <H5>You will be redirected to <b>{domain}</b> in 3 seconds</H5>
+              <p>If you don't want to wait, <a href={url}>click here</a>.</p>
             </div>
           </Row>
         </Layout>
