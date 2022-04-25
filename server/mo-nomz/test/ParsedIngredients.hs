9a48f0c50e8ab852426476c02a2b835de95f0ac3
Dan Fithian <danielf@vitalbio.com>
2022-04-25 13:23:45 -0400

    Try to fix test

--- server/mo-nomz/test/ParsedIngredients.hs
+++ server/mo-nomz/test/ParsedIngredients.hs
@@ -235,7 +235,7 @@ sallysBakingSteps =
   , Step "Coarsely chop the chocolate. Place butter into a medium heat-proof bowl, then add chopped chocolate on top. Microwave on high in 10 second increments, stirring after each until completely smooth. Set aside."
   , Step "Whisk the flour, confectioners\8217 sugar, and salt together in a small bowl. Whisk the eggs and egg yolks together until combined in another small bowl. Pour the flour mixture and eggs into the bowl of chocolate. Slowly stir everything together using a rubber spatula or wooden spoon. If there are any lumps, gently use your whisk to rid them. The batter\194 will be slightly thick."
   , Step "Spoon chocolate batter evenly into each prepared ramekin or muffin cup."
-  , Step "Place ramekins onto a baking sheet and bake for 12-14 minutes until the sides appear solid and firm– the tops will still look soft. *If baking in a muffin pan, the cakes only take about 8-10 minutes."
+  , Step "Place ramekins onto a baking sheet and bake for 12-14 minutes until the sides appear solid and firm\8211 the tops will still look soft. *If baking in a muffin pan, the cakes only take about 8-10 minutes."
   , Step "Allow to cool for 1 minute, then cover each with an inverted plate and turn over. Use an oven mitt because those ramekins are hot! The cakes should release easily from the ramekin. *If you used a muffin pan, use a spoon to release the cakes from the pan and place each upside down on plates."
   , Step "Add toppings. Serve immediately."
   ]
