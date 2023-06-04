b0789eca8167e58bb02cb6f139e8f06efe6899ad
Dan Fithian <daniel.m.fithian@gmail.com>
2023-06-04 13:11:45 -0400

    Add ad banner to recipe detail view

--- client/app/mo-nomz/Recipe/RecipeDetailController.swift
+++ client/app/mo-nomz/Recipe/RecipeDetailController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class RecipeDetailController: UIViewController, UITextViewDelegate, RecipeTagDelegate {
+    @IBOutlet weak var banner: UIView!
     @IBOutlet weak var name: UITextView!
     @IBOutlet weak var star1: UIButton!
     @IBOutlet weak var star2: UIButton!
@@ -169,6 +170,9 @@ class RecipeDetailController: UIViewController, UITextViewDelegate, RecipeTagDel
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? BannerController, segue.identifier == "embedBanner" {
+            vc.height = banner.constraints.filter({ $0.identifier == "height" }).first
+        }
         guard let r = recipe else { return }
         if let vc = segue.destination as? RecipeDetailTagController, segue.identifier == "embedTags" {
             tagVc = vc
