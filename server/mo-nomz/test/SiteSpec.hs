c56955834085675bd84b6d4932b341c0e05622bd
Dan Fithian <daniel.m.fithian@gmail.com>
2023-07-29 13:36:34 -0400

    Try to update test

--- server/mo-nomz/test/SiteSpec.hs
+++ server/mo-nomz/test/SiteSpec.hs
@@ -128,7 +128,7 @@ spec env = describe "Scrape" $ do
     it "handles everydayannie" $ scrapeAndParseConfig defCfg "https://everydayannie.com/2020/12/28/raspberry-cheesecake-streusel-bars/"
     -- 500 internal server error
     xit "handles notwithoutsalt" $ scrapeAndParseConfig defCfg "http://notwithoutsalt.com/brussels-sprout-green-apple-slaw-pickled-cranberries/"
-    it "handles chefspencil" $ scrapeAndParseConfig (defCfg { requiredSteps = 0 }) "https://www.chefspencil.com/recipe/carrot-tarte-tatin/"
+    it "handles chefspencil" $ scrapeAndParseConfig defCfg "https://www.chefspencil.com/marinated-skirt-steak/"
     it "handles sweetandsavorymeals" $ scrapeAndParseConfig defCfg "https://sweetandsavorymeals.com/air-fryer-eggplant/"
     it "handles eatwell101" $ scrapeAndParseConfig (defCfg { requiredSteps = 0 }) "https://www.eatwell101.com/garlic-butter-chicken-bites-asparagus-recipe"
     it "handles bbcgoodfood" $ scrapeAndParseConfig defCfg "https://www.bbcgoodfood.com/recipes/challah/"
